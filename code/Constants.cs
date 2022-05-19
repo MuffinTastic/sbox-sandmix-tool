ac77395f9869a3582eb337022a1501e14f04e18a
MuffinTastic <muffintastic32@gmail.com>
2022-05-19 12:52:40 -0700

    change repo url

--- code/Constants.cs
+++ code/Constants.cs
@@ -5,7 +5,7 @@ public static class SandMixTool
 	public const string ProjectName = "s&mix";
 	public const string ProjectTagline = "Dynamic node-based sound mixer for s&box!";
 	public const string ProjectVersion = "0.0.1";
-	public const string ProjectRepoURL = "https://www.example.com/";
+	public const string ProjectRepoURL = "https://github.com/MuffinTastic/sbox-sandmix-tool";
 	public static readonly string[] Authors = { "MuffinTastic" };
 
 	public const string FileExtension = "smix";

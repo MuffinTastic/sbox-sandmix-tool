f0d313dd036a733bfe2b1148e84fc022bea6ef1e
MuffinTastic <muffintastic32@gmail.com>
2022-10-13 17:51:16 -0700

    switch file filters to use resource extension constants

--- code/Constants.cs
+++ code/Constants.cs
@@ -1,4 +1,5 @@
 ﻿using Sandbox;
+using SandMix;
 
 namespace SandMixTool;
 
@@ -9,12 +10,20 @@ public static class SandMixTool
 	public const string ProjectVersion = "0.0.1";
 	public const string ProjectRepoURL = "https://github.com/MuffinTastic/sbox-sandmix-tool";
 	public static readonly string[] Authors = { "MuffinTastic" };
+	
+	public const string FindFileFilter =
+		$"All {ProjectName} (*.{MixGraphResource.FileExtension} *.{EffectResource.FileExtension});;" +
+		$"{ProjectName} mixgraph (*.{MixGraphResource.FileExtension});;" +
+		$"{ProjectName} effect (*.{EffectResource.FileExtension});;" +
+		$"All files (*.*)";
 
-	public const string MixGraphFileExtension = "smix";
-	public const string EffectFileExtension = "smixefct";
-	public const string FindFileFilter = $"All {ProjectName} (*.{MixGraphFileExtension} *.{EffectFileExtension});;{ProjectName} mixgraph (*.{MixGraphFileExtension});;{ProjectName} effect (*.{EffectFileExtension});;All files (*.*)";
-	public const string SaveMixGraphFilter = $"{ProjectName} mixgraph (*.{MixGraphFileExtension});;All files (*.*)";
-	public const string SaveEffectFilter = $"{ProjectName} effect (*.{EffectFileExtension});;All files (*.*)";
+	public const string SaveMixGraphFilter =
+		$"{ProjectName} mixgraph (*.{MixGraphResource.FileExtension});;" +
+		$"All files (*.*)";
+
+	public const string SaveEffectFilter =
+		$"{ProjectName} effect (*.{EffectResource.FileExtension});;" +
+		$"All files (*.*)";
 
 	[ConVar.Engine( "smix_tool_debug" )]
 	public static bool Debug { get; set; } = false;
